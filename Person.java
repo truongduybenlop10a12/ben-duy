2a9bc604fe80f1c01fa8b7384168f20a2d66705e
truongduybenlop10a12 <truongduyben123.az@gmail.com>
2024-12-17 09:40:50 +0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,27 @@
+package ben.dev;
+
+import java.util.Iterator;
+
+public class Person {
+	/**
+	 * Bien instance / bien doi tuong
+	 */
+	public String name;
+	public int old;
+	
+	public static String nameSchool = "Hue UNI";
+	
+	public Person() {
+		// TODO Auto-generated constructor stub
+		// old la bien cuc bo nam trong ham khoi tao
+		this.name = "";
+		this.old = 10 ;
+	}
+	    
+	public void show() {
+	System.out.println();	 
+	}
+	}
+	
+
+
