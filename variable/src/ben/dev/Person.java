11f56e91399c3439190c0b7c24835d0781f6725e
truongduybenlop10a12 <truongduyben123.az@gmail.com>
2024-12-17 09:01:18 +0700

    Add files via upload

---
+++ variable/src/ben/dev/Person.java
@@ -0,0 +1,23 @@
+package ben.dev;
+
+import java.util.Iterator;
+
+public class Person {
+    public Person() {
+		// TODO Auto-generated constructor stub
+    	// old la bien cuc bo nam trong ham khoi tao
+	} 
+ 
+	
+    public void show()    {
+     int old;
+     for (Iterator iterator = collection.iterator(); iterator.hasNext();) {
+		  
+	}
+ 
+    	
+    	
+    	
+    	
+    	
+    	
