93a1f363bf15adfc1ebc34654c6a17009ce70982
truongduybenlop10a12 <truongduyben123.az@gmail.com>
2024-12-17 09:38:35 +0700

    Add files via upload

--- MainApp.java
+++ MainApp.java
@@ -4,6 +4,23 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 9;
+		Person p = new Person();
+		p.name = "Person 1";
+		
+		p.nameSchool = "HaNoi UNI";
+		
+		Person p2 = new Person();
+		p2.name = "Person 2";
+		
+	    p2.nameSchool = "DaNang UNI";
+	    
+	    
+		System.out.println(p.name);
+		System.out.println(p.nameSchool);
+		
+		System.out.println(p.name);
+		System.out.println(p2.nameSchool);
 
 	}
 
