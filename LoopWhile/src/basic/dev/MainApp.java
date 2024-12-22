5958eca708038a3f4795b6e98e46d9065cf46357
truongduybenlop10a12 <truongduyben123.az@gmail.com>
2024-12-22 15:44:13 +0700

    Add files via upload

---
+++ LoopWhile/src/basic/dev/MainApp.java
@@ -0,0 +1,21 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		int tong = 0;
+		do {
+			i++;
+			tong += i;
+		}while(i <= 100);
+		
+		System.out.println("tong = " + tong);
+		System.out.println("i =	" + i);
+		
+		}
+
+	}
+
+
