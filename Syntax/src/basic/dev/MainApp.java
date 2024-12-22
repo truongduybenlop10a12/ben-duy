9a8896196bcda4e4d0fb2c905b68b6a06b0e37c0
truongduybenlop10a12 <truongduyben123.az@gmail.com>
2024-12-22 15:46:46 +0700

    Add files via upload

---
+++ Syntax/src/basic/dev/MainApp.java
@@ -0,0 +1,21 @@
+package basic.dev;
+
+public class MainApp {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int number;
+		String nameSchool;
+	}
+	/*
+	 * tên phương thức tuân theo quy tắc camelCase
+	 */
+	public void show() {
+		
+	}
+}
+	
+		
+
+
+	
