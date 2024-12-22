a2d4c8d81bd1bc599bc41747f8f631fc737b2a82
truongduybenlop10a12 <truongduyben123.az@gmail.com>
2024-12-22 10:04:47 +0700

    Add files via upload

---
+++ LoopFor/src/basic/dev/MainApp.java
@@ -0,0 +1,19 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub=
+		int tong  = 0;
+		
+		//for(bt1; bt2; bt3) { cau lenh; }
+		for(int i = 0; i <= 10; i++) {
+			if(i == 50)
+				continue;
+			tong += i;
+			
+		}
+	}
+
+}
+	
