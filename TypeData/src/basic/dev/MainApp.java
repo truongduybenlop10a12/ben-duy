e8b5945fa287c55ccb413bf29413e056c222b813
truongduybenlop10a12 <truongduyben123.az@gmail.com>
2024-12-22 08:30:32 +0700

    Add files via upload

---
+++ TypeData/src/basic/dev/MainApp.java
@@ -0,0 +1,54 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        byte b = 9;
+        System.out.println(b);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        
+        short s1 = b;
+        
+        short s = 10;
+        System.out.println(s);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        
+        int in = s;
+        
+        
+        int i = 10;
+        System.out.println(i);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        
+        
+        long l = 100;
+        System.out.println(l);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        
+        
+        float f = 100;
+        System.out.println(f);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+        
+        
+        
+        double d = 100;
+        System.out.println(d);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        
+        
+        char c = 'A';
+        System.out.println(c);
+        
+        boolean bool = true;
+        System.out.println(bool);
+	}
+
+}
