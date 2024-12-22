4a9255af0197e71f256e83d260289b2706f8c366
truongduybenlop10a12 <truongduyben123.az@gmail.com>
2024-12-22 15:42:47 +0700

    Add files via upload

---
+++ ConditionIf/src/basic/dev/MainApp.java
@@ -0,0 +1,19 @@
+package basic.dev;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		
+		if(a > 5)
+			System.out.println("A lon hon 5");
+		
+		System.out.println("ket thuc chuong trinh");									
+	}
+
+}
