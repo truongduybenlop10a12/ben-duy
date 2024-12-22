f78fd11f3865bf2b8b3c8f2ba5938b29ddc29d2a
truongduybenlop10a12 <truongduyben123.az@gmail.com>
2024-12-22 15:41:32 +0700

    Add files via upload

---
+++ IOJava/src/basic/dev/MainApp.java
@@ -0,0 +1,20 @@
+package basic.dev;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		long l = sc.nextLong();
+		float f = sc.nextFloat();
+		double d = sc.nextDouble();
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);	
+	}
+
+}
